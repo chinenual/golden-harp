803b26b01093e9273d42c394967043f5124059cc
Steve Tynor <chinenual@gmail.com>
2023-04-13 18:14:35 -0400

    update firmware script to reflect Iasos laptop layout

--- golden-harp-adapter/upload-hex.bat
+++ golden-harp-adapter/upload-hex.bat
@@ -1,4 +1,4 @@
-SET COMPORT=COM1
+SET COMPORT=COM3
 SET HEXFILE=golden-harp-adapter-1_4.ino.hex
-"C:\Program Files\Arduino\hardware\tools\avr/bin/avrdude" -C"C:\Program Files\Arduino\hardware\tools\avr/etc/avrdude.conf" -v -patmega328p -carduino -P%COMPORT% -b57600 -D -Uflash:w:%HEXFILE%:i 
+"C:\Arduino\arduino-1.3.0\hardware\tools\avr\bin\avrdude" -C"C:\Arduino\arduino-1.3.0\hardware\tools\avr\etc\avrdude.conf" -v -patmega328p -carduino -P%COMPORT% -b57600 -D -Uflash:w:%HEXFILE%:i 
 
